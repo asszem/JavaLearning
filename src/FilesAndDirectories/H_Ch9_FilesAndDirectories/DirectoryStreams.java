97392b11e335a9052e5f76ec81779e5d8a217eba
András <asszem78@gmail.com>
2017-03-25 00:40:18 +0100

    Change DirectoryStream<Path> to Try-With-Resources, update Note

--- src/FilesAndDirectories/H_Ch9_FilesAndDirectories/DirectoryStreams.java
+++ src/FilesAndDirectories/H_Ch9_FilesAndDirectories/DirectoryStreams.java
@@ -12,16 +12,22 @@ import java.nio.file.Paths;
  */
 public class DirectoryStreams {
 
+	public static void main(String[] args) {
+		Path utv = Paths.get("E:\\javaTest");
+		listDir(utv, null);
+		listDir(utv, "*.txt");
+	}
+
 	//Method to list content of directory with optional Filter parameter
 	//use "null" if filter is not needed
 	public static void listDir(Path dirPath, String filter) {
 		System.out.println("Mappa kilistázása");
 		System.out.printf("Mappa neve:%s%nFilter:%s%n", dirPath, filter);
-		try {
-			DirectoryStream<Path> mappaStream = filter==null?Files.newDirectoryStream(dirPath):Files.newDirectoryStream(dirPath, filter);
-			int counter=0;
+		//try-with-resources block to automatically close the directory stream
+		try (DirectoryStream<Path> mappaStream = (filter == null ? Files.newDirectoryStream(dirPath) : Files.newDirectoryStream(dirPath, filter))) {
+			int counter = 0;
 			for (Path p : mappaStream) {
-				System.out.println("#"+(++counter)+" "+p);
+				System.out.println("#" + (++counter) + " " + p);
 			}
 			System.out.println("Lista vége.");
 		} catch (IOException e) {
@@ -29,8 +35,8 @@ public class DirectoryStreams {
 		}
 	}
 
-		//List only .txt files in the dir
-		/*
+	//List only .txt files in the dir
+	/*
 		TABLE 9-5: Character Sequences for Filtering Directory Streams
 		CHARACTER SEQUENCE EFFECT
 		* Matches zero or more characters of a name without crossing a directory boundary,
@@ -48,10 +54,5 @@ public class DirectoryStreams {
 		character in a bracket expression.
 		{} Specifi es a set of subpatterns that can be matched where the subpatterns are
 		separated by commas. For example, {exe, txt} matches either exe or txt.
-		 */
-	public static void main(String[] args) {
-		Path utv = Paths.get("E:\\javaTest");
-		listDir(utv, null);
-		listDir(utv, "*.txt");
-	}
+	 */
 }
