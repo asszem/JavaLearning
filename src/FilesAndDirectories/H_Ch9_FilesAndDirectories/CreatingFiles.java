04985ba28dbc13eaa0ad8d633ed6c9c646e79565
András <asszem78@gmail.com>
2017-03-24 17:14:53 +0100

    completed the creating files sample class

--- src/FilesAndDirectories/H_Ch9_FilesAndDirectories/CreatingFiles.java
+++ src/FilesAndDirectories/H_Ch9_FilesAndDirectories/CreatingFiles.java
@@ -1,12 +1,28 @@
-
 package FilesAndDirectories.H_Ch9_FilesAndDirectories;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
- * 
+ *
  * @author Andras Olah (olahandras78@gmail.com)
  */
 public class CreatingFiles {
+
 	public static void main(String[] args) {
-		
+		String pathStr = "E:\\javaFileOpTest";
+		Path path = Paths.get(pathStr, "createFileinThisFolder", "createFile.txt");
+		try {
+			System.out.println("Input path: " + path);
+			Files.createDirectories(path.getParent()); // Create parent
+			Files.createFile(path);
+		} catch (FileAlreadyExistsException e) {
+			System.err.println("File not created.\n" + e);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
 	}
 }
