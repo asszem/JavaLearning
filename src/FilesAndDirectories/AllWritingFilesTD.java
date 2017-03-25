e30faf0da7b23655ccd600bd6c162a4c6adaf1b1
András <asszem78@gmail.com>
2017-03-25 22:41:34 +0100

    Created bufferedWriter method and test, Notes updated

--- src/FilesAndDirectories/AllWritingFilesTD.java
+++ src/FilesAndDirectories/AllWritingFilesTD.java
@@ -13,7 +13,7 @@ public class AllWritingFilesTD {
 
 	public static void main(String[] args) {
 		Path testPath = Paths.get("E:\\javaFileOpTest");
-		//<editor-fold desc="Test case: open a file to write with newOutputStream()">
+		//<editor-fold desc="newOutputStream test">
 		//call the method that opens a file and write to it 
 		if (false) {
 			AllWritingFiles.writingWithOutputStream(testPath.resolve("outputStreamWriteTest.txt"), 12345);
@@ -23,9 +23,21 @@ public class AllWritingFilesTD {
 			AllWritingFiles.writingWithOutputStreamWithOptions(testPath.resolve("outputStreamAppend.txt"), 12345);
 		}
 		//</editor-fold>
-		//<editor-fold desc="">
+		//<editor-fold desc="BufferedWriter tests">
+		if (false) {
+			System.out.println("Test case: write with Buffered Writer");
+			String testString = "Árvíztűrő tükörfúrógép";
+			Path filename = testPath.resolve("BufferedWriterTest.txt");
+			AllWritingFiles.writingWithBufferedWriter(filename, testString, "default");
+			AllWritingFiles.writingWithBufferedWriter(filename, testString, "default");
+			AllWritingFiles.writingWithBufferedWriter(filename, testString, "default");
+			waitForEnter("Check the file, is the content 3 times or 1 time written? Should be 1");
+			AllWritingFiles.writingWithBufferedWriter(filename, testString, "APPEND");
+			AllWritingFiles.writingWithBufferedWriter(filename, testString, "APPEND");
+			AllWritingFiles.writingWithBufferedWriter(filename, testString, "append");
+			waitForEnter("Check the file, is the content 3 times or 1 time written? Should be 3");
+		}
 		//</editor-fold>
-
 		//<editor-fold desc="Testing of ++index%2 and 1%2">
 		if (false) {
 			int counter = 0;
@@ -37,11 +49,14 @@ public class AllWritingFilesTD {
 			}
 		}
 		//</editor-fold>
-	}
 
-	public static void waitForEnter() {
+		//<editor-fold desc="">
+		//</editor-fold>
+	}//main
+
+	public static void waitForEnter(String input) {
 		try {
-			System.out.println("Press Enter");
+			System.out.println(input);
 			System.in.read();
 		} catch (IOException e) {
 			System.err.println(e);
