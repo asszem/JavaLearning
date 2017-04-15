4dc5356f8b85dda70b355b1f0857ac14e790b351
András <asszem78+laptop@gmail.com>
2017-04-15 22:28:33 +0200

    Complete Ch11,Ex2 readPrimes method with Junit test (validating last read item)

--- src/HortonExercises/Ch11/Ex2/Ex2_Andras.java
+++ src/HortonExercises/Ch11/Ex2/Ex2_Andras.java
@@ -12,9 +12,15 @@ package HortonExercises.Ch11.Ex2;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.READ;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,10 +30,72 @@ import java.util.logging.Logger;
  */
 public class Ex2_Andras {
 
+	Path sourceFile = Paths.get("J:\\Writing Files\\primesMixed.bin");
+	long sourceFileSize;
+	String[] readedStrings;
+	long[] readedLongs;
+	ArrayList readedStringsArrayList = new ArrayList();
+	ArrayList readedLongsArrayList = new ArrayList();
+	static final int LONG_BYTES = 8;
+	static final int CHAR_BYTES = 2;
+	static final int DOUBLE_BYTES = 8;
+
+	public void readPrimes() {
+		if (!Files.exists(sourceFile)) {
+			throw new IllegalStateException("File does not exists!");
+		}
+		int currentStringLength;
+		String currentString;
+		long currentLong;
+		try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(sourceFile, READ)) {
+			ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+//			byteBuffer.position(byteBuffer.capacity()); //preparing for .compact
+byteBuffer.flip();
+			sourceFileSize = fileChannel.size();
+			while (true) { //exit if EOF reached
+				//Read to the bytebuffer
+				if (fileChannel.read(byteBuffer.compact()) == -1) {
+					break; //EOF reached, exit the loop
+				}
+				byteBuffer.flip();
+				while (true) { //exit if not enough data in buffer
+//check if there is next double
+					if (byteBuffer.remaining() < DOUBLE_BYTES) {
+						break; //skip the buffer reading, continue with next file read
+					}
+					currentStringLength = (int) byteBuffer.getDouble();
+//check if there is enough for next string
+					if (byteBuffer.remaining() < currentStringLength * CHAR_BYTES) {
+						byteBuffer.position(byteBuffer.position() - DOUBLE_BYTES);
+						break;
+					}
+					byte[] byteArrayForStrings = new byte[currentStringLength * CHAR_BYTES];
+					byteBuffer.get(byteArrayForStrings);
+					ByteBuffer byteBufferForByteArray = ByteBuffer.wrap(byteArrayForStrings);
+					currentString = byteBufferForByteArray.asCharBuffer().toString();
+//check if there is enough for next long
+					if (byteBuffer.remaining() < LONG_BYTES) {
+						byteBuffer.position(byteBuffer.position() - (DOUBLE_BYTES + (currentStringLength * CHAR_BYTES)));
+						break;
+					}
+					currentLong = byteBuffer.getLong();
+//generate the next string
+				readedStringsArrayList.add(currentString);
+				readedLongsArrayList.add(currentLong);
+				}//end byteBuffer reading loop
+			}//end file channel reading loop
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void displayPrime() {
+
+	}
+
 	public static int getKeyboardInput() {
 		int result = 0;
-
-		BufferedInputStream inputStream = new BufferedInputStream(System.in);
 		System.out.println("Enter a number:");
 		StreamTokenizer tokenizer = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 		try {
@@ -41,7 +109,10 @@ public class Ex2_Andras {
 	}
 
 	public static void main(String[] args) {
-		int input = getKeyboardInput();
-		System.out.println("The input was:" + input);
+		Ex2_Andras testObject = new Ex2_Andras();
+		testObject.readPrimes();
+
+//		int input = getKeyboardInput();
+//		System.out.println("The input was:" + input);
 	}
 }
---
+++ test/HortonExercises/Ch11/Ex2/Ex2_AndrasTest.java
@@ -0,0 +1,70 @@
+package HortonExercises.Ch11.Ex2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author Andras Olah (olahandras78@gmail.com)
+ */
+public class Ex2_AndrasTest {
+
+	static Ex2_Andras testObject;
+
+	public Ex2_AndrasTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+		testObject = new Ex2_Andras();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of readPrimes method, of class Ex2_Andras.
+	 * Last result format:
+	 * [99] Sting length:[011] String:[prime = 541] Long:[541]
+	 * Let's just check the last long value
+	 * 
+	 */
+	@Test
+	public void testReadPrimes() {
+		testObject.readPrimes();
+		long expResult=541L;
+		//get the last item 
+		int t=testObject.readedLongsArrayList.size();
+		int lastItemIndex=testObject.readedLongsArrayList.size()-1;
+		long actResult=(long)testObject.readedLongsArrayList.get(lastItemIndex);
+		assertEquals(expResult, actResult);
+	}
+
+	/**
+	 * Test of getKeyboardInput method, of class Ex2_Andras.
+	 */
+	@Ignore
+	@Test
+	public void testGetKeyboardInput() {
+		System.out.println("getKeyboardInput");
+		int expResult = 422;
+		int result = Ex2_Andras.getKeyboardInput();
+		assertEquals(expResult, result);
+	}
+
+
+}
