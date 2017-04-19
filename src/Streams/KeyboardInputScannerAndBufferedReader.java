8910206358916f2c90a9ed08c0e9100e5353ffc9
András <asszem78@gmail.com>
2017-04-19 20:54:58 +0200

    Update KeyboardInput class with input/output stream parameter inputs and Encoding class with encoder

--- src/Streams/KeyboardInputScannerAndBufferedReader.java
+++ src/Streams/KeyboardInputScannerAndBufferedReader.java
@@ -1,8 +1,15 @@
 package Streams;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.io.StreamTokenizer;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
@@ -11,6 +18,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class KeyboardInputScannerAndBufferedReader {
+
+	/*
+	Supported Encodng - Charset codes:
+	https://docs.oracle.com/javase/8/docs/technotes/guides/intl/encoding.doc.html
+	 */
+	public static String charset = "Windows-1250";
 
 	//ÁRVÍZTŰRŐ compliant
 	public static String getStringInputUsingScanner() {
@@ -25,17 +38,50 @@ public class KeyboardInputScannerAndBufferedReader {
 
 	//ÁRVÍZTŰRŐ compliant
 	public static String getStringUsingBufferedReader() {
-		String charSet="ISO8859_1";
-		String returnString=null;
+		String charSet = "ISO8859_1";
+		String returnString = null;
 		try {
-			BufferedReader bufferedReader= new BufferedReader(new InputStreamReader (System.in, charSet));
-			returnString=bufferedReader.readLine();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, charSet));
+			returnString = bufferedReader.readLine();
 		} catch (UnsupportedEncodingException ex) {
 			ex.printStackTrace();
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 		return returnString;
+	}
+
+	/**
+	 * Reads a line from the provided InputStream and displays to the provided outputtream
+*	
+* The input and output streams need to be opened before calling the method
+* The outputstream parameter need to be manually updated if System.out or OutputStreamWriter is to be used
+	 *
+	 * @param inputStream System.in for console input, ByteArrayInputStream for Junit mock input
+	 * @param outputStream System.out for console output, OutputStreamWriter for file output
+	 * @return a String that was provided to the inputStream
+	 */
+	public static String getStringUsingScannerWithInputOutputStreamParameter(InputStream inputStream, String outputStreamChoice){
+		System.out.printf("Charset:%s, output stream:%s",charset, outputStreamChoice
+		System.out.println("Charset for input: " + charset);
+		System.out.print("Enter something:");
+		Scanner sc = new Scanner(inputStream, charset);
+		String result = sc.nextLine();
+		switch (outputStreamChoice){
+			case "console":
+				break;
+		}
+		try {
+			//cast the outputStream to a PrintStream so that it can be displayed to the console
+//		PrintStream printStream = (PrintStream) outputStream;
+//		printStream.println("The input was:" + result); //use the provided output instead of System.out
+			outputStream.write(result);
+			outputStream.flush();
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return result;
 	}
 
 	public static int getNumberUsingStreamTokenizer() {
@@ -74,8 +120,13 @@ public class KeyboardInputScannerAndBufferedReader {
 		return inputCharacter;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
 //		get_INT_OnlyUsingScanner();
-System.out.println(getStringUsingBufferedReader());
+//		System.out.println(getStringUsingBufferedReader());
+//		getStringUsingScannerWithInputOutputStreamParameter(System.in, System.out);
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("Árvíztűrő tükörfúrógép".getBytes(charset));
+		String outputFile = "J:\\outputstreamwriter.txt";
+		OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(outputFile), charset);
+		getStringUsingScannerWithInputOutputStreamParameter(byteArrayInputStream, osw);
 	}
 }
