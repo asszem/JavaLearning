836c172770dd90c7dc1bb02284a35cfb647561bc
András <asszem78@gmail.com>
2017-05-07 14:20:46 +0200

    Update HashMapOperations class and notes description of HashMaps
    - Two private inner classes created to demonstrate how to
        - use a class as a key (override equals, hashcode, compareto, toString)
        - genereate hashcodes for classes when their field are other class references
        - use HashMaps with my own objects as Key:Value
        - remove items based on their Key:Value
    - Update the Collections Framework.txt notes file with detailed description of HashMaps

--- src/ClassesAndObjects/Practice/H_Ch14_CollectionsFramework/TryPhoneBook/Person.java
+++ src/ClassesAndObjects/Practice/H_Ch14_CollectionsFramework/TryPhoneBook/Person.java
@@ -3,52 +3,53 @@ package ClassesAndObjects.Practice.H_Ch14_CollectionsFramework.TryPhoneBook;
 import java.io.*;
 
 public class Person implements Comparable<Person>, Serializable {
-  // Constructor
-  public Person(String firstName, String surname) {
-    this.firstName = firstName;
-    this.surname = surname;
-  }
 
-  // Read a person from the keyboard
-  public static Person readPerson() {
-    String firstName = null;
-    String surname = null;
-    try {
-      System.out.print("Enter first name: ");
-      firstName = keyboard.readLine().trim();
-      System.out.print("Enter surname: ");
-      surname = keyboard.readLine().trim();
-    } catch(IOException e) {
-      System.err.println("Error reading a name.");
-      e.printStackTrace();
-      System.exit(1);
-    }
-    return new Person(firstName,surname);
-  }
+	private String firstName;                                            // First name of person
+	private String surname;                                              // Second name of person
+	private static final long serialVersionUID = 1001L;
+	private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-  @Override
-  public boolean equals(Object person) {
-    return compareTo((Person)person) == 0;
-  }
+	// Constructor
+	public Person(String firstName, String surname) {
+		this.firstName = firstName;
+		this.surname = surname;
+	}
 
-  @Override
-  public int hashCode() {
-    return 7*firstName.hashCode()+13*surname.hashCode();
-  }
+	// Read a person from the keyboard
+	public static Person readPerson() {
+		String firstName = null;
+		String surname = null;
+		try {
+			System.out.print("Enter first name: ");
+			firstName = keyboard.readLine().trim();
+			System.out.print("Enter surname: ");
+			surname = keyboard.readLine().trim();
+		} catch (IOException e) {
+			System.err.println("Error reading a name.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return new Person(firstName, surname);
+	}
 
-  @Override
-  public String toString() {
-    return firstName + " " + surname;
-  }
+	@Override
+	public boolean equals(Object comparedToPerson) {							//Uses the compareTo method
+		return this.compareTo((Person) comparedToPerson) == 0;					//Evaulates TRUE if compareTo returns 0
+	}
 
-  // Compare Person objects
-  public int compareTo(Person person) {
-    int result = surname.compareTo(person.surname);
-    return result == 0 ? firstName.compareTo(person.firstName) : result;
-  }
+	@Override
+	public int hashCode() {
+		return 7 * firstName.hashCode() + 13 * surname.hashCode();				//Generates hashcode based on first name and last name
+	}
 
-  private String firstName;                                            // First name of person
-  private String surname;                                              // Second name of person
-  private static final long serialVersionUID = 1001L;
-  private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+	// Compare Person objects
+	public int compareTo(Person comparedToPerson) {
+		int result = this.surname.compareTo(comparedToPerson.surname);							//Compares Person objects by surname field
+		return result == 0 ? this.firstName.compareTo(comparedToPerson.firstName) : result;		//Compares firstname if surname is the same
+	}
+
+	@Override
+	public String toString() {
+		return firstName + " " + surname;
+	}
 }
