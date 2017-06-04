c06c4aab620d39488cb6fd017bdeb2d09d6fd08e
András <asszem78@gmail.com>
2017-06-04 16:39:46 +0200

    WIP Fonts

---
+++ src/GUI/Practice/Fonts_Sample.java
@@ -0,0 +1,32 @@
+package GUI.Practice;
+
+import java.awt.Font;
+
+import GUI.Window_Collection;
+
+public class Fonts_Sample {
+
+	public static void main(String[] args) {
+		Font[] fonts = Window_Collection.getAllFonts();
+//		System.out.println(java.util.Arrays.toString(fonts));
+		int counter=0;
+		for (Font font:fonts){
+			System.out.println("-------------");
+			System.out.println("Font number: "+counter++);
+			System.out.println(font);
+			System.out.println("-------------");
+		}
+		Font myTimes = fonts[357].deriveFont(Font.BOLD, 20);
+		System.out.println(myTimes);
+		
+		String[] fontnames = Window_Collection.getAllFontNames();
+		counter=0;
+		System.out.println("Printing out fontnames only");
+		for (String font:fontnames){
+			System.out.println("Font number: "+(counter++)+" "+ font);
+		}
+		Font myTimes2 = new Font(fontnames[195], Font.ITALIC | Font.BOLD, 12);
+		System.out.println(myTimes2);
+	}
+
+}
