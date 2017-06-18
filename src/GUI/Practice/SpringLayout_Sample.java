ae0444b72af00b6704d186319448dc6bc7ab3ac9
András <asszem78@gmail.com>
2017-06-18 16:12:46 +0200

    WIP CH17, Ex6, Spring Layout

--- src/GUI/Practice/SpringLayout_Sample.java
+++ src/GUI/Practice/SpringLayout_Sample.java
@@ -22,27 +22,27 @@ public class SpringLayout_Sample {
 		mainJFrame.setLocationRelativeTo(null);
 
 		// Create a Container
-		Container mainContent = mainJFrame.getContentPane();
+		Container mainContentPane = mainJFrame.getContentPane();
 
 		// Create the Layout Object
 		SpringLayout springLayout = new SpringLayout();
 
 		// Set the layout manager for Container
 		// mainJFrame.setLayout(springLayout); //Automatically sets the content for the ContentPane
-		mainContent.setLayout(springLayout);
+		mainContentPane.setLayout(springLayout);
 
 		// Create Button Components
-		JButton button1 = createButton("B1 - ", new Dimension(300,200));
-		JButton button2 = createButton("B2 - ", new Dimension(300,200));
-
+		JButton button1 = createButton("B1 - ", new Dimension(300, 200));
+		JButton button2 = createButton("B2 - ", new Dimension(300, 200));
 
 		// Add the Component to the Container
-		mainContent.add(button1);
-		mainContent.add(button2);
+		mainContentPane.add(button1);
+		mainContentPane.add(button2);
 
 		// Get the constraints for the component
 		SpringLayout.Constraints button1Constraints = springLayout.getConstraints(button1);
 		SpringLayout.Constraints button2Constraints = springLayout.getConstraints(button2);
+		SpringLayout.Constraints contentPaneConstraint = springLayout.getConstraints(mainContentPane);
 
 		// Create a Spring object
 		Spring springX = Spring.constant(10, 100, 200);			// min, pref, max
@@ -55,23 +55,45 @@ public class SpringLayout_Sample {
 		// Assign the Spring object to the Component's Constraint
 		button1Constraints.setX(springX);
 		button1Constraints.setY(springY);
-//		button1Constraints.setWidth(springWidth);
-//		button1Constraints.setHeight(springHeight);
-		//Assigning a new constraint will overwrite previously settings
-//		button1Constraints.setWidth(Spring.width(button1)); //Automatically assign the width of the component
-//		button1Constraints.setHeight(Spring.height(button1)); //Automatically assign the width of the component
+		 button1Constraints.setWidth(springWidth);
+		 button1Constraints.setHeight(springHeight);
 
-		//Set the constraint directly - overwrites previous settings
-//		button1Constraints.setConstraint(SpringLayout.NORTH, Spring.constant(20,200,300));
-//		button2Constraints.setConstraint(SpringLayout.NORTH, Spring.constant(20,200,300));
-		
-		//Set the constraint relatively to each other
-		Spring springBetween = Spring.constant(30,40,50);
+		// Assigning a new constraint will overwrite previously settings
+		// button1Constraints.setWidth(Spring.width(button1)); //Automatically assign the width of the component
+		// button1Constraints.setHeight(Spring.height(button1)); //Automatically assign the width of the component
+
+		// Set the constraint directly - overwrites previous settings
+		// button1Constraints.setConstraint(SpringLayout.NORTH, Spring.constant(20,200,300));
+		// button2Constraints.setConstraint(SpringLayout.NORTH, Spring.constant(20,200,300));
+
+		// Set the constraint relatively to each other of two components
+		Spring springBetween = Spring.constant(50);
+		// DEPENDENT component || Spring || REFERENCE component
+		// Add a 50px strut between the BOTTOM of button1 and the TOP of button2
 		springLayout.putConstraint(SpringLayout.NORTH, button2, springBetween, SpringLayout.SOUTH, button1);
-	
-	
+		// Add a 50px strut between the RIGHT of button1 and the LEFT of button2
+		springLayout.putConstraint(SpringLayout.WEST, button2, springBetween, SpringLayout.EAST, button1);
+
+		// Set the constraints relative to the Content Pane
+		displayConstraint(button2Constraints);
+		// Set the constraint relative to the button1's WEST and North Edges
+		int paddingSize=20;
 		
-		
+		//Set the EAST (right) side of content pane relative to the button2
+		Spring eastSpring=Spring.sum(button2Constraints.getConstraint(SpringLayout.EAST), Spring.constant(paddingSize));
+		contentPaneConstraint.setConstraint(SpringLayout.EAST,eastSpring);
+
+		//Set the NORTH (bottom) side of the content pane relative to button2
+		Spring southSpring=Spring.sum(button2Constraints.getConstraint(SpringLayout.SOUTH), Spring.constant(paddingSize));
+		contentPaneConstraint.setConstraint(SpringLayout.SOUTH, southSpring);
+		System.out.println(button2Constraints.getConstraint(SpringLayout.SOUTH).getValue());
+		// Sets the constraint of the pane, regardless of its components
+		// contentPaneConstraint.setConstraint(SpringLayout.WEST, Spring.constant(20,200,300));
+		// contentPaneConstraint.setConstraint(SpringLayout.SOUTH, Spring.constant(20,200,300));
+
+		// Adjust the size of the frame to the content. SpringLayout does not set it automatically
+		mainJFrame.pack();
+
 		// Set the jFrame visible
 		mainJFrame.setVisible(true);
 	}
@@ -80,7 +102,18 @@ public class SpringLayout_Sample {
 		String buttonText = text + (int) dimension.getWidth() + "x" + (int) dimension.getHeight();
 		JButton button = new JButton(buttonText);
 		button.setPreferredSize(dimension);
+		button.setMinimumSize(dimension);
+		button.setMaximumSize(dimension);
 		return button;
+	}
+
+	public static void displayConstraint(SpringLayout.Constraints constraint) {
+		System.out.println("West: " + constraint.getConstraint(SpringLayout.WEST));
+		System.out.println("East: " + constraint.getConstraint(SpringLayout.EAST));
+		System.out.println("North: " + constraint.getConstraint(SpringLayout.NORTH));
+		System.out.println("South: " + constraint.getConstraint(SpringLayout.SOUTH));
+		// System.out.println(constraint.getConstraint(SpringLayout.WIDTH));
+		// System.out.println(constraint.getConstraint(SpringLayout.HEIGHT));
 	}
 
 	public static void displaySprings(Spring... springs) {
