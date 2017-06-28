048b39a1af717759936ca38e100ac84d4dafee19
András <asszem78@gmail.com>
2017-06-28 23:52:04 +0200

    Complete MaxMirror unit test for compare mirrors.

--- practice/codingBat/array_3/maxMirror.java
+++ practice/codingBat/array_3/maxMirror.java
@@ -1,6 +1,10 @@
 /*
  * 
-We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array, the same group appears in reverse order. For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part). Return the size of the largest mirror section found in the given array.
+We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array,
+ the same group appears in reverse order. For example, the largest mirror section in 
+ {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+
+ Return the size of the largest mirror section found in the given array.
 
 maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
 maxMirror([1, 2, 1, 4]) → 3
@@ -11,11 +15,14 @@ package codingBat.array_3;
 public class maxMirror {
 
 	public static int mirrorr(int[] array) {
+		System.out.println("Initial array:");
+		System.out.println(java.util.Arrays.toString(array));
 		int result = 0;
+		int maxRightIndex = array.length - 1;
 		// Loop 1 - increase mirrorSize up until length of array, starting from 2 - mirrorSize
 		for (int mirrorSize = 2; mirrorSize < array.length; mirrorSize++) {
 
-			// Loop 2 - walk array from left to right, with mirrorSize to get the leftMirror values - leftIndex (from 0 to array.lenght-mirrorSize)
+			// Loop 2 - walk array from left to right, with mirrorSize to get the leftIndex (from 0 to array.lenght-mirrorSize)
 			for (int leftIndex = 0; leftIndex < array.length - (mirrorSize); leftIndex++) {
 
 				// Loop 3 - create and set leftMirror values -> leftMirror[mirrorSize]
@@ -23,23 +30,27 @@ public class maxMirror {
 				for (int i = 0; i < leftMirror.length; i++) {
 					leftMirror[i] = array[leftIndex + i];
 				}// End loop 3
-					// Loop 4 - walk backward from right to left and check if there is a mirror match - rightIndex (from array.length until 0+mirrorSize
-				for (int rightIndex = array.length - 1; rightIndex > mirrorSize; rightIndex--) {
+
+				// Loop 4 - walk backward from right to left and check if there is a mirror match - rightIndex (from array.length until 0+mirrorSize
+				for (int rightIndex = maxRightIndex; rightIndex >= mirrorSize; rightIndex--) {
 					int[] rightMirror = new int[mirrorSize];
+
 					// Loop 5 - create and set rightMirror values
 					for (int i = 0; i < rightMirror.length; i++) {
 						rightMirror[i] = array[rightIndex - i];
 					}// End loop 5
 						// Compare left & right mirrors
-					boolean mirrorsEqual = true;
-					for (int i = 0; i < leftMirror.length; i++) {
-						if (leftMirror[i] != rightMirror[(rightMirror.length - 1) - i]) {
-							mirrorsEqual = false;
-						}
-					}
+					boolean mirrorsEqual = false;
+//					if (leftIndex + (mirrorSize-1) != rightIndex) {				// Itself does not count as a mirror
+//						System.out.println(java.util.Arrays.toString(leftMirror) + " -- "
+//								+ java.util.Arrays.toString(rightMirror));
+//						System.out.printf("ms=%d, li=%d, ri=%d%n",mirrorSize, leftIndex, rightIndex);
+						mirrorsEqual=compareMirror(leftMirror, rightMirror);
+//					}
 					if (mirrorsEqual) {
-						System.out.println(java.util.Arrays.toString(leftMirror) + " -- "
-								+ java.util.Arrays.toString(rightMirror));
+//						System.out.println(java.util.Arrays.toString(leftMirror) + " -- "
+//								+ java.util.Arrays.toString(rightMirror));
+						result=mirrorSize;
 					}
 				} // end Loop 4
 			} // end loop 2
@@ -48,10 +59,32 @@ public class maxMirror {
 		return result;
 	}
 
+	public static boolean compareMirror(int[] a, int[] b) {
+		int alength=a.length;
+		int blength=b.length;
+		for (int i = 0; i < a.length; i++) {
+			int curA=a[i];
+			int curB=b[(blength-1)-i];
+			if (curA!=curB)
+				return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
-		int[] test = { 1, 2, 3, 1, 2 }; // 4
-		// int[] test = { 1, 2, 3, 4, 5, 4, 3, 2, 1 }; // 4
-		mirrorr(test);
+		int[] test1 = { 1, 2, 3, 4, 5 }; // 0
+		int[] test2 = {1, 2, 3, 8, 9, 3, 2, 1}; // 3
+		int[] test3 = { 7, 1, 2, 9, 7, 2, 1 }; // 2
+		int[] test4 = { 1, 2, 1, 4 }; // 3
+		System.out.println(mirrorr(test1));
+		System.out.println(mirrorr(test2));
+		System.out.println(mirrorr(test3));
+		System.out.println(mirrorr(test4));
+		/*
+		 * maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3 
+		 * maxMirror([1, 2, 1, 4]) → 3 
+		 * maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
+		 */
 
 	}
 }
---
+++ test/codingBat/array_3/maxMirrorTest.java
@@ -0,0 +1,43 @@
+package codingBat.array_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class maxMirrorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCompareMirror_invalidMirror() {
+		int[] a={1,2,3,4};
+		int[] b={5,6,7,8};
+		assertFalse(maxMirror.compareMirror(a, b));
+	}
+	@Test
+	public void testCompareMirror_validMirror() {
+		int[] a={1,2,3,4};
+		int[] b={4,3,2,1};
+		assertTrue(maxMirror.compareMirror(a, b));
+	}
+
+}
