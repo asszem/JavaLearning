0062e65ab3f004908d2b1e146d7a177a3db3f311
András <asszem78@gmail.com>
2017-07-04 10:43:38 +0200

    Complete EventListeners, JButton, JMenuBar sample classes

--- src/GUI/JMenuBar_Sample.java
+++ src/GUI/JMenuBar_Sample.java
@@ -1,6 +1,5 @@
 package GUI;
 
-
 import javax.swing.ButtonGroup;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
@@ -12,23 +11,28 @@ import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import static java.awt.event.InputEvent.*;
 
-public class JMenuBar_Sample {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class JMenuBar_Sample implements ActionListener, ItemListener {
 
 	private static JFrame window;
+	private static JMenuBar mainMenuBar = new JMenuBar();
+	private static JMenu jMenu = new JMenu("Menu");
+	private static JMenu jSubMenu = new JMenu("SubMenu");
+	private static JMenuItem menuItem1 = new JMenuItem("MenuItem 1");
+	private static JMenuItem menuItem2 = new JMenuItem("MenuItem 2");
+	private static JMenuItem quitMenuItem = new JMenuItem("Quit");
+	private static JCheckBoxMenuItem jcbMenuItem1 = new JCheckBoxMenuItem("Checkbox 1");
+	private static JCheckBoxMenuItem jcbMenuItem2 = new JCheckBoxMenuItem("Checkbox 2");
+	private static JRadioButtonMenuItem jrbMenuItem1 = new JRadioButtonMenuItem("Button 1");
+	private static JRadioButtonMenuItem jrbMenuItem2 = new JRadioButtonMenuItem("Button 2");
+	private static ButtonGroup radioButtonGroup = new ButtonGroup();
 
 	private static void addMenuBar(JFrame jFrame) {
-		JMenuBar mainMenuBar = new JMenuBar();
 		jFrame.setJMenuBar(mainMenuBar);
-		JMenu jMenu = new JMenu("Menu");
-		JMenu jSubMenu = new JMenu("SubMenu");
-
-		JMenuItem menuItem1 = new JMenuItem("MenuItem 1");
-		JMenuItem menuItem2 = new JMenuItem("MenuItem 2");
-		JCheckBoxMenuItem jcbMenuItem1 = new JCheckBoxMenuItem("Checkbox 1");
-		JCheckBoxMenuItem jcbMenuItem2 = new JCheckBoxMenuItem("Checkbox 2");
-		JRadioButtonMenuItem jrbMenuItem1 = new JRadioButtonMenuItem("Button 1");
-		JRadioButtonMenuItem jrbMenuItem2 = new JRadioButtonMenuItem("Button 2");
-		ButtonGroup radioButtonGroup = new ButtonGroup();
 		radioButtonGroup.add(jrbMenuItem1);
 		radioButtonGroup.add(jrbMenuItem2);
 
@@ -39,18 +43,34 @@ public class JMenuBar_Sample {
 		jMenu.add(jrbMenuItem1);
 		jMenu.addSeparator();
 		jMenu.add(jSubMenu);
+		jMenu.add(quitMenuItem);
 		jSubMenu.add(menuItem2);
 		jSubMenu.add(jcbMenuItem2);
 		jSubMenu.add(jrbMenuItem2);
 
 		// Create shortcuts for Menu - Alt-M will open Menu
 		jMenu.setMnemonic('M');
-		
-		//Create accelerator for menu items
+
+		// Create accelerator for menu items
 		menuItem1.setAccelerator(KeyStroke.getKeyStroke('M', (CTRL_DOWN_MASK | ALT_DOWN_MASK)));
 		menuItem2.setAccelerator(KeyStroke.getKeyStroke('N', (CTRL_DOWN_MASK | ALT_DOWN_MASK)));
 
-	}
+		// Add action listener to menu
+		JMenuBar_Sample menuActionListener = new JMenuBar_Sample();
+		menuItem1.addActionListener(menuActionListener);
+		menuItem2.addActionListener(menuActionListener);
+		jcbMenuItem1.addItemListener(menuActionListener);
+		
+		
+		quitMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Quit selected. Exiting.");
+				System.exit(0);
+			}
+		});
+	}//end of addMenuBar method
 
 	public static void createJFrame() {
 		window = new JFrame();
@@ -69,5 +89,26 @@ public class JMenuBar_Sample {
 				createJFrame();
 			}
 		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object eventSource = e.getSource();
+		if (eventSource == menuItem1) {
+			System.out.println("Menu item 1 selected");
+		}
+		if (eventSource == menuItem2) {
+			System.out.println("Menu item 2 selected");
+		}
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		Object eventSource = e.getSource();
+		if (eventSource == jcbMenuItem1) {
+			System.out.print("CheckBox1 item state changed. isSelected: ");
+			System.out.println(jcbMenuItem1.isSelected());
+		}
+		
 	}
 }
