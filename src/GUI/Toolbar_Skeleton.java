54ce983b91ebed913e1ac58ccdb52a1318f526f3
András <asszem78@gmail.com>
2017-07-08 10:08:07 +0200

    Complete Toolbar Skeleton class

---
+++ src/GUI/Toolbar_Skeleton.java
@@ -0,0 +1,173 @@
+package GUI;
+
+import static java.awt.event.InputEvent.CTRL_DOWN_MASK;
+import static javax.swing.Action.LARGE_ICON_KEY;
+import static javax.swing.Action.NAME;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+
+public class Toolbar_Skeleton {
+
+	private JFrame window;
+	private ToolbarAction tbaOpen;
+	private ToolbarAction tbaSave;
+	private ToolbarAction tbaQuit;
+	private JToolBar toolBar;
+
+	// Toolbar icons
+	public final static String imagePath = "J:/Horton Assets/Sketcher/";
+	public final static Icon OPEN24 = new ImageIcon(imagePath + "Open24.gif");
+	public final static Icon SAVE24 = new ImageIcon(imagePath + "Save24.gif");
+
+	public void createGUI() {
+		window = createJFrame();
+		createActionObjects();
+		setIconForToolbarObjects();
+		createToolbar();
+		addToolbarItems();
+		window.setVisible(true);
+	}
+
+	public void createActionObjects() {
+		tbaOpen = new ToolbarAction("Open", 'O', CTRL_DOWN_MASK);
+		tbaSave = new ToolbarAction("Save", 'S', CTRL_DOWN_MASK);
+		tbaQuit = new ToolbarAction("Quit", 'Q', CTRL_DOWN_MASK);
+	}
+
+	public void setIconForToolbarObjects() {
+		tbaOpen.putValue(LARGE_ICON_KEY, OPEN24);
+		tbaSave.putValue(LARGE_ICON_KEY,  SAVE24);
+		// no icon gif for tbaQuit
+	}
+
+	public void createToolbar() {
+		toolBar = new JToolBar();
+		toolBar.setRollover(true);
+		toolBar.setFloatable(true);
+		Container content = window.getContentPane();
+		content.add(toolBar, BorderLayout.NORTH); // Default layout mgr is BorderLayout for content pane
+	}
+
+	public void addToolbarItems() {
+		toolBar.add(tbaOpen);
+		toolBar.add(tbaSave);
+		toolBar.add(tbaQuit);
+		toolBar.addSeparator();
+		createToolBarButton(tbaOpen);
+		createToolBarButton(tbaSave);
+		createToolBarButton(tbaQuit);
+		toolBar.addSeparator();
+	}
+
+	public void createToolBarButton(Action action) {
+		JButton button = new JButton(action);                              // Create a JButton object from Action object
+		button.setBorder(BorderFactory.createCompoundBorder(               // Add button border
+				new EmptyBorder(2, 5, 5, 2),                               // Outside border - empty, as it is used as a padding from the edges of toolbar
+				BorderFactory.createRaisedBevelBorder()));                 // Inside border
+		button.setHideActionText(false);                                    // No label on the button
+		toolBar.add(button);
+	}
+
+	public JFrame createJFrame() {
+		JFrame window = new JFrame("Toolbar Skeleton");
+		Toolkit theKit = window.getToolkit();                             // Get the window toolkit
+		Dimension wndSize = theKit.getScreenSize();                       // Get screen size
+		window.setSize(wndSize.width / 2, wndSize.height / 2);   	      // Set window size
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return window;
+	}
+
+	public static void main(String[] args) {
+		Toolbar_Skeleton appInstance = new Toolbar_Skeleton();
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				appInstance.createGUI();
+			}
+		});
+	}
+
+	@SuppressWarnings("serial")
+	class ToolbarAction extends AbstractAction {
+
+		// Basic constructor
+		public ToolbarAction(String name) {
+			super(name);
+		}
+
+		// Constructor for menu item accellerators
+		public ToolbarAction(String name, char shortcut, int keyStrokeModifier) {
+			super(name);
+			putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(shortcut, keyStrokeModifier)); // Set property by key:value
+
+			// Now find the character to underline
+			int index = name.toUpperCase().indexOf(shortcut);
+			if (index != -1) {
+				putValue(DISPLAYED_MNEMONIC_INDEX_KEY, index); // Set property by key:value
+			}
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object eventSource=e.getSource();
+			if (eventSource instanceof JButton){
+				System.out.println("Event source: JButton");
+				Action currentAction= ((JButton) eventSource).getAction();
+				if (currentAction==tbaOpen){
+					System.out.println("Open pressed");
+				}
+				if (currentAction==tbaSave){
+					System.out.println("Save pressed");
+				}
+				if (currentAction==tbaQuit){
+					System.out.println("Quit pressed");
+					System.exit(0);
+				}
+			}
+		}
+	}
+
+	//Method is not used, but included here only for reference
+	private void setMenuItemCorrespondToolbarItem(JMenu sourceMenu, Object eventSource) {
+		// Check if eventSource is JButton
+		if (eventSource instanceof JButton) {
+			// Create a Jbutton object reference for the eventSource object
+			JButton tempButton = (JButton) eventSource;
+			// Get the Action object for the event source - this is why the tempButton was created
+			Action sourceActionObject = tempButton.getAction();
+			// Iterate through available JMenu items
+			for (int i = 0; i < sourceMenu.getItemCount(); i++) {
+				// Create a temp reference for the current menu item
+				JMenuItem tempItem=sourceMenu.getItem(i);
+				// Set the state checked if the current JMenu item's Action object equals with the event source
+				// Action object
+				boolean isSameAction=tempItem.getAction().equals(sourceActionObject);
+				tempItem.setSelected(isSameAction);
+				if (isSameAction){
+					System.out.println("Menu item set to "+sourceActionObject.getValue(NAME));
+				}
+
+			}
+		}
+	}
+
+} // end of Toolbar_Skeleton class
